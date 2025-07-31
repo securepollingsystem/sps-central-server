896a29da70dfeb87d335f3a8410690df1c57c871
jerkey <jerkey@g-cipher.net>
2025-07-30 17:35:30 -0700

    reinit project with typescript, no router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import preactLogo from '../../assets/preact.svg';
+import { render } from 'preact';
+
+import preactLogo from './assets/preact.svg';
 import './style.css';
 
-export function Home() {
+export function App() {
 	return (
-		<div class="home">
+		<div>
 			<a href="https://preactjs.com" target="_blank">
 				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
 			</a>
@@ -37,3 +39,5 @@ function Resource(props) {
 		</a>
 	);
 }
+
+render(<App />, document.getElementById('app'));
