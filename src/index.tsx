7f5c77f260cc6483f781c7b9768f241c04f013bf
jerkey <jerkey@g-cipher.net>
2025-08-15 15:02:57 -0700

    change tabs to 2-spaces throughout project

--- src/index.tsx
+++ src/index.tsx
@@ -4,40 +4,40 @@ import pageLogo from './assets/secure polling.svg';
 import './style.css';
 
 export function App() {
-	return (
-		<div>
-			<a href="https://securepollingsystem.org/" target="_blank">
-				<img src={pageLogo} alt="secure polling system logo" height="300" width="300" />
-			</a>
-			<h1>Secure Polling System Administrator Console</h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
-			</section>
-		</div>
-	);
+  return (
+    <div>
+      <a href="https://securepollingsystem.org/" target="_blank">
+        <img src={pageLogo} alt="secure polling system logo" height="300" width="300" />
+      </a>
+      <h1>Secure Polling System Administrator Console</h1>
+      <section>
+        <Resource
+          title="Learn Preact"
+          description="If you're new to Preact, try the interactive tutorial to learn important concepts"
+          href="https://preactjs.com/tutorial"
+        />
+        <Resource
+          title="Differences to React"
+          description="If you're coming from React, you may want to check out our docs to see where Preact differs"
+          href="https://preactjs.com/guide/v10/differences-to-react"
+        />
+        <Resource
+          title="Learn Vite"
+          description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
+          href="https://vitejs.dev"
+        />
+      </section>
+    </div>
+  );
 }
 
 function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
+  return (
+    <a href={props.href} target="_blank" class="resource">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </a>
+  );
 }
 
 render(<App />, document.getElementById('app'));
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-	server: {
-		port: 8992,
-		allowedHosts: ['stemgrid.org','registrar.securepollingsystem.org','registrar.securepollingsystem.com']
-	}
+  plugins: [preact()],
+  server: {
+    port: 8992,
+    allowedHosts: ['stemgrid.org','registrar.securepollingsystem.org','registrar.securepollingsystem.com']
+  }
 });
